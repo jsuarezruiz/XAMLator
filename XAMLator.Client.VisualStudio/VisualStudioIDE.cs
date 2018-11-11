4b9833c27cddf542d1ff0c4637408cbc37768b66
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-11-11 19:12:03 +0100

    Init Visual Studio Windows extension Added WPF Sample

---
+++ XAMLator.Client.VisualStudio/VisualStudioIDE.cs
@@ -0,0 +1,67 @@
+﻿using EnvDTE;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XAMLator.Client.VisualStudio
+{
+    public class VisualStudioIDE : IIDE
+    {
+        readonly DTE _dte;
+
+        public event EventHandler<DocumentChangedEventArgs> DocumentChanged;
+
+        public VisualStudioIDE(DTE dte)
+        {
+            _dte = dte;
+        }
+
+        public void MonitorEditorChanges()
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (_dte == null)
+                throw new InvalidOperationException("XAMLanator not initialized.");
+
+            var textEditorEvents = _dte.Events.TextEditorEvents;
+            textEditorEvents.LineChanged += OnLineChanged;
+        }
+
+        private void OnLineChanged(TextPoint startPoint, TextPoint endPoint, int hint)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            var textDocument = startPoint?.Parent;
+            if (textDocument == null) return;
+
+            var document = startPoint.Parent.Parent;
+            if (document == null) return;
+
+            EmitDocumentChanged(document);
+        }
+
+        public Task RunTarget(string targetName)
+        {
+            // TODO:
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string error, Exception ex = null)
+        {
+            Log.Error(error);
+            MessageBox.Show(error);
+        }
+
+        void EmitDocumentChanged(Document document)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            TextDocument doc = (TextDocument)(_dte.ActiveDocument.Object("TextDocument"));
+            var point = doc.StartPoint.CreateEditPoint();
+            string docText = point.GetText(doc.EndPoint);
+
+            Log.Information($"XAML document changed {document.Name}");
+            DocumentChanged?.Invoke(this, new DocumentChangedEventArgs(document.FullName, docText, null, null));
+        }
+    }
+}
---
+++ XAMLator.Client.VisualStudio/XAMLatorPackage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace XAMLator.Client.VisualStudio
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    // The following line will schedule the package to be initialized when a solution is being opened
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class XAMLatorPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "c3dd450f-35c5-44e5-ab40-51960e8c9acb";
+
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // Since this package might not be initialized until after a solution has finished loading,
+            // we need to check if a solution has already been loaded and then handle it.
+            bool isSolutionLoaded = await IsSolutionLoadedAsync();
+
+            if (isSolutionLoaded)
+            {
+                HandleOpenSolution();
+            }
+
+            // Listen for subsequent solution events.
+            Microsoft.VisualStudio.Shell.Events.SolutionEvents.OnAfterOpenSolution += HandleOpenSolution;
+        }
+
+        private async Task<bool> IsSolutionLoadedAsync()
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            // Check if a solution has loaded.
+            var solService = await GetServiceAsync(typeof(SVsSolution)) as IVsSolution;
+
+            Assumes.Present(solService);
+            ErrorHandler.ThrowOnFailure(solService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object value));
+
+            return value is bool isSolOpen && isSolOpen;
+        }
+
+        private async void HandleOpenSolution(object sender = null, EventArgs e = null)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(DisposalToken);
+            DTE dte = (DTE)await GetServiceAsync(typeof(DTE));
+
+            // Initialize Visual Studio XAMLatorMonitor.
+            XAMLatorMonitor.Init(new VisualStudioIDE(dte));
+
+            // Start monitoring.
+            XAMLatorMonitor.Instance.StartMonitoring();
+        }
+    }
+}
